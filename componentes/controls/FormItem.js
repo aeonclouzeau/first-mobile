2e7019054f500ba08f1b3869dc44b97b97ee646e
Aeon Clouzeau <aeonruizc@gmail.com>
2024-01-23 12:13:41 -0600

    add styles to FormItem component

--- componentes/controls/FormItem.js
+++ componentes/controls/FormItem.js
@@ -2,11 +2,32 @@ import { View, Text, TextInput, StyleSheet } from "react-native";
 import Colors from "../../constants/Colors";
 import Fonts from "../../constants/Fonts";
 
-export default function FormItem(params) {
+export default function FormItem(label) {
 	return (
-		<View>
-			<Text>probando</Text>
-			<TextInput>text</TextInput>
+		<View style={styles.container}>
+			{label && <Text style={styles.label}>{label}</Text>}
+			<TextInput style={styles.input} />
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		marginBottom: 10,
+		width: "100%",
+	},
+	label: {
+		color: Colors.oldSilver,
+		fontSize: Fonts.size.small,
+		fontFamily: Fonts.family.regular,
+		textAlign: "left",
+	},
+	input: {
+		borderBottomColor: Colors.cinnabar,
+		borderBottomWidth: 2,
+		color: Colors.jet,
+		fontSize: Fonts.size.normal,
+		paddingBottom: 5,
+		paddingTop: 5,
+	},
+});
