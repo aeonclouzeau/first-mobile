421bb6cbf5dae011ff0e7744b115e8443a73d67f
Aeon Clouzeau <aeonruizc@gmail.com>
2024-01-23 12:22:58 -0600

    add style to header component

--- componentes/layout/Header.js
+++ componentes/layout/Header.js
@@ -3,16 +3,50 @@ import { Ionicons, EvilIcons } from "@expo/vector-icons";
 import Colors from "../../constants/Colors";
 import Fonts from "../../constants/Fonts";
 
-export default function Header(params) {
-	<View>
-		<TouchableOpacity>
-			<Ionicons name="arrow-back-outline" size={40} color="black"></Ionicons>
-		</TouchableOpacity>
-		<View>
-			<Image source={require("../../assets/icon.png")} />
+export default function Header(title) {
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity>
+				<Ionicons name="arrow-back-outline" size={40} color="black"></Ionicons>
+			</TouchableOpacity>
+			<View style={styles.logoContainer}>
+				{title && <Text style={styles.title}>{title}</Text>}
+				<Image style={styles.logo} source={require("../../assets/icon.png")} />
+			</View>
+			<TouchableOpacity>
+				<EvilIcons name="cart" size={40} color="black"></EvilIcons>
+			</TouchableOpacity>
 		</View>
-		<TouchableOpacity>
-			<EvilIcons name="cart" size={40} color="black"></EvilIcons>
-		</TouchableOpacity>
-	</View>;
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: "center",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingBottom: 30,
+		paddingHorizontal: 30,
+		paddingTop: 30,
+	},
+	logo: {
+		height: 100,
+		width: 100,
+	},
+	logoContainer: {
+		alignItems: "center",
+	},
+	title: {
+		backgroundColor: Colors.white,
+		borderColor: Colors.platinum,
+		borderRadius: 20,
+		borderWidth: 1,
+		borderBottomWidth: 2,
+		color: Colors.jet,
+		fontFamily: Fonts.family.bold,
+		fontSize: Fonts.size.normal,
+		paddingHorizontal: 10,
+		paddingBottom: 5,
+		marginTop: 10,
+	},
+});
