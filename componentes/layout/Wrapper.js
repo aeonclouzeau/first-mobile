d0a8fec00cdafab8f8d7292189a359bc214afb4c
Aeon Clouzeau <aeonruizc@gmail.com>
2024-01-23 12:42:03 -0600

    fix wrapper problem

--- componentes/layout/Wrapper.js
+++ componentes/layout/Wrapper.js
@@ -1,6 +1,6 @@
 import { View, ScrollView } from "react-native";
 
-export default function Wrapper(children) {
+export default function Wrapper({ children }) {
 	return (
 		<View>
 			<ScrollView>{children}</ScrollView>
